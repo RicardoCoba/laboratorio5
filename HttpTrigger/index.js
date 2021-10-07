4d6999baa05777fb1caea72742f0f04b5996cf1a
enrique <nohunenrique@gmail.com>
2021-10-06 21:29:34 -0500

    Practica cuatro

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -2,12 +2,17 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Welcome, " + name
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    if(name){
+        context.res = {
+            status: 200,
+            body: "Welcome, " + name
+        };
+    }else{
+        context.res = {
+            status: 400, /* Defaults to 200 */
+            body:"Pass a name in the query string or in the request body"
+        };
+    }
+   
+    
 }
