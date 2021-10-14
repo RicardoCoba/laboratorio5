2533924812f91cb6f0410fe8016402c23e166002
RicardoCoba <ricardo.coba@live.com>
2021-10-13 22:12:44 -0500

    pruebas rendimiento

--- HttpTrigger/index.test.js
+++ HttpTrigger/index.test.js
@@ -3,7 +3,7 @@ const context= require('../testing/Context');
 
 test('Http trigger example' , async()=> {
     const request = {
-        query: {name: 'Enrique'}
+        query: {name: 'Ricardo'}
     };
  var interations = 1000000;
  console.time('FUNCTION #1');
@@ -12,6 +12,6 @@ test('Http trigger example' , async()=> {
     httpFuction(context, request);
  }
     console.timeEnd('FUNCTION #1')
-    expect(context.res.body).toEqual('Welcome, Enrique');
+    expect(context.res.body).toEqual('Welcome, Ricardo');
     expect(context.log.mock.calls.length).toBe(1000000);
 });
--- README.md
+++
@@ -1,1 +0,0 @@
-# practica-laboratorio-uno
